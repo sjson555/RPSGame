1155184ab6231c224c9823bd8950c983c1a64fda
sungjun son <o_nj_u@naver.com>
2024-07-10 22:51:26 +0900

    가위바위보 앱 만들기 / 코드설정(3)

---
+++ RPSGame/RPS.swift
@@ -0,0 +1,15 @@
+//
+//  Rps.swift
+//  RPSGame
+//
+//  Created by 손성준 on 7/10/24.
+//
+
+import Foundation
+
+// 원시값(raw value)
+enum Rps: Int {
+    case rock
+    case paper
+    case scissors
+}
